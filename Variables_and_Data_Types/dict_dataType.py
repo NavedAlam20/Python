06885cfd0d4afeacbafebb2259aad9143550497b
Naved Alam <navedalam5665@gmail.com>
2026-02-05 23:39:28 +0530

    Dictionary Data Type

---
+++ Variables_and_Data_Types/dict_dataType.py
@@ -0,0 +1,12 @@
+# initialize empty dictionary
+d = {}
+
+d = { 1: 'welcome',
+         2: 'to',
+         3: 'python'
+}
+print(d) # {1: 'welcome', 2: 'to', 3: 'python'}
+
+# creating dictionary using dict() constructor
+d1 = dict({1: 'welcome', 2: 'to', 3: 'python'})
+print(d1) # {1: 'welcome', 2: 'to', 3: 'python'}
