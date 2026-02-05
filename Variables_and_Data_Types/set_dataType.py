b94128ee37ee90da757564f564a43d5d76279c4a
Naved Alam <navedalam5665@gmail.com>
2026-02-05 23:31:54 +0530

    Set Data Type

---
+++ Variables_and_Data_Types/set_dataType.py
@@ -0,0 +1,8 @@
+# initializing empty set
+s1 = set()
+
+s1 = set("WelcomeToPython")
+print("Set with the use of String: ", s1) # {'P', 't', 'c', 'T', 'h', 'l', 'm', 'n', 'W', 'o', 'e', 'y'}
+
+s2 = set(["Welcome", "To", "Python"])
+print("Set with the use of List: ", s2) # {'Welcome', 'Python', 'To'}
