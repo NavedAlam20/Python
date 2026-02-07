d213caded1738cbafe2ea294094b46c64a2f2931
Naved Alam <navedalam5665@gmail.com>
2026-02-07 18:28:34 +0530

    How To Take Input

---
+++ Variables_and_Data_Types/input.py
@@ -0,0 +1,20 @@
+name = input("Enter your name: ")
+print("Hello,", name, "! Welcome!")
+
+# Take Multiple Input in one line
+
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
+
+# Change the Type of Input
+
+color = input("What color is rose?: ")
+print(color)
+
+n = int(input("How many roses?: "))
+print(n)
+
+price = float(input("Price of each rose?: "))
+print(price)
