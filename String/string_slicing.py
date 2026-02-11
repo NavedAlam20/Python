568a675dbcd79e1ad5c8c4ed10ee1180049bc5f6
Naved Alam <navedalam5665@gmail.com>
2026-02-11 21:36:26 +0530

    String Slicing

---
+++ String/string_slicing.py
@@ -0,0 +1,5 @@
+s = "ThisIsString"
+print(s[1:4])    # characters from index 1 to 3
+print(s[:3])     # from start to index 2
+print(s[3:])     # from index 3 to end
+print(s[::-1])   # reverse string
