2fd2bf94427ba66e9ce2413e4376b1683ccb10e3
Naved Alam <navedalam5665@gmail.com>
2026-02-13 23:08:09 +0530

    This code fix the first letter and replace a word

---
+++ String/updating_str.py
@@ -0,0 +1,5 @@
+s = "hello student"
+s1 = "H" + s[1:]                   # update first character
+s2 = s.replace("student", "Python")  # replace word
+print(s1) # Hello student
+print(s2) # hello Python
