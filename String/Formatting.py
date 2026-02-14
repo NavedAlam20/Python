cfe482854e6e09b3ba7ac462e67a20e2ce23c65f
Naved Alam <navedalam5665@gmail.com>
2026-02-14 23:14:15 +0530

    Formatting Strings

---
+++ String/Formatting.py
@@ -0,0 +1,8 @@
+#Using f-strings
+name = "Naved"
+age = 21
+print(f"Name: {name}, Age: {age}") # Name: Naved, Age: 21
+
+#Using format()
+s = "My name is {} and I am {} years old.".format("Naved", 21)
+print(s) # My name is Naved and I am 21 years old.
