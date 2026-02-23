cef47786da926045397239620d30393e36578f22
Naved Alam <navedalam5665@gmail.com>
2026-02-23 23:39:51 +0530

    This program checks whether a user agrees to the terms or not.

---
+++ Conditional_Statements/Exercise/Q6.py
@@ -0,0 +1,7 @@
+var = int(input("Enter Y for YES and N for NO:"))
+if (var == 'Y' or var == 'y'):
+    print("YOU SAID YES")
+elif (var == 'N' or var == 'n'):
+    print("YOU SAID NO")
+else:
+    print("INVALID INPUT")
