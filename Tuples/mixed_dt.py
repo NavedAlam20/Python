fccca443579fde80e8c37a478458da94402e81e3
Naved Alam <navedalam5665@gmail.com>
2026-03-11 22:59:02 +0530

    Creating a Tuple with Mixed Datatypes.

---
+++ Tuples/mixed_dt.py
@@ -0,0 +1,12 @@
+tup = (5, 'Welcome', 7, 'python')
+print(tup) #(5, 'Welcome', 7, 'python')
+
+# Creating a Tuple with nested tuples
+tup1 = (0, 1, 2, 3)
+tup2 = ('python', 'python')
+tup3 = (tup1, tup2)
+print(tup3) #((0, 1, 2, 3), ('python', 'python'))
+
+# Creating a Tuple with repetition
+tup1 = ('python',) * 3
+print(tup1) #('python', 'python', 'python')
