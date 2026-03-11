9cbf1321076863dbb6f46ddeeebc78b7ba6183ce
Naved Alam <navedalam5665@gmail.com>
2026-03-11 23:04:07 +0530

    Accessing of Tuples

---
+++ Tuples/Accessing.py
@@ -0,0 +1,16 @@
+# Accessing Tuple with Indexing
+tup = tuple("Python")
+print(tup[0]) #P
+
+# Accessing a range of elements using slicing
+print(tup[1:4]) #yth
+print(tup[:3]) #Pyt
+
+# Tuple unpacking
+tup1 = ("Welcome", "To", "Python")
+
+# This line unpack values of Tuple1
+a, b, c = tup1
+print(a) #Welcome
+print(b) #To
+print(c) #Python
