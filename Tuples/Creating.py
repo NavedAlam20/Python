63a721ce0bdc9704dbcdacdc63f88f9a64f29ef1
Naved Alam <navedalam5665@gmail.com>
2026-03-11 22:55:37 +0530

    Creating a Tuple

---
+++ Tuples/Creating.py
@@ -0,0 +1,14 @@
+tup = ()
+print(tup) #()
+
+# Using String
+tup = ('Geeks', 'For')
+print(tup) #('Geeks', 'For')
+
+# Using List
+li = [1, 2, 4, 5, 6]
+print(tuple(li)) #(1, 2, 4, 5, 6)
+
+# Using Built-in Function
+tup = tuple('Geeks')
+print(tup) #('G', 'e', 'e', 'k', 's')
