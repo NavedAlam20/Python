e543bc5d2613b299dd906e42fde3a5eda84b2059
Naved Alam <navedalam5665@gmail.com>
2026-03-12 22:08:10 +0530

    Slicing of Tuple

---
+++ Tuples/Slicing.py
@@ -0,0 +1,10 @@
+tup = tuple('HIPYTHON')
+
+# Removing First element
+print(tup[1:]) #('I','P','Y','T','H','O','N')
+
+# Reversing the Tuple
+print(tup[::-1]) #('N','O','H','T','Y','P','I','H')
+
+# Printing elements of a Range
+print(tup[4:6]) #('T','H')
