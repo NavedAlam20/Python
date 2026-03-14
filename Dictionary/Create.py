32f73fc5f9c5abffe7ca5b7cd5b69a0b98317c0d
Naved Alam <navedalam5665@gmail.com>
2026-03-14 21:42:39 +0530

    Creating a Dictionary

---
+++ Dictionary/Create.py
@@ -0,0 +1,10 @@
+d1 = {1: 'Welcome',
+      2: 'To',
+      3: 'Python'}
+print(d1) #{1: 'Welcome', 2: 'To', 3: 'Python'}
+
+# using dict() constructor
+d2 = dict(a = "Welcome", 
+          b = "To", 
+          c = "Python")
+print(d2) #{1: 'Welcome', 2: 'To', 3: 'Python'}
