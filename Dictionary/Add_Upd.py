6dec83c1ca839816165565ed898b804432c33fae
Naved Alam <navedalam5665@gmail.com>
2026-03-14 21:55:03 +0530

    Adding and Updating Dictionary Items

---
+++ Dictionary/Add_Upd.py
@@ -0,0 +1,10 @@
+d = {1: 'Welcome',
+     2: 'To', 
+     3: 'Python'}
+
+# Adding a new key-value pair
+d["age"] = 22
+
+# Updating an existing value
+d[1] = "Python dict"
+print(d) #{1: 'Python dict', 2: 'To', 3: 'Python', 'age': 22}
