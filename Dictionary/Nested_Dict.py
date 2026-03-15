5e9b76abfb92506167c6f0a8c1e6a73ca48f6d75
Naved Alam <navedalam5665@gmail.com>
2026-03-15 22:30:43 +0530

    dictionary that contains another dictionary as one of its values

---
+++ Dictionary/Nested_Dict.py
@@ -0,0 +1,7 @@
+d = {1: 'Hi', 
+     2: 'Naved', 
+     3: {'A': 'Welcome', 
+         'B': 'To', 
+         'C': 'Python'}
+    }
+print(d) #{1: 'Hi', 2: 'Naved', 3: {'A': 'Welcome', 'B': 'To', 'C': 'Python'}}
