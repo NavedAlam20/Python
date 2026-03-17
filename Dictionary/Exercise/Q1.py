c43e719b8ea6bea6556857fb062d8f5384878637
Naved Alam <navedalam5665@gmail.com>
2026-03-17 21:39:55 +0530

    Dictionary with Keys having Multiple Inputs

---
+++ Dictionary/Exercise/Q1.py
@@ -0,0 +1,9 @@
+points = {}
+
+points[(1, 2)] = "A"
+points[(3, 4)] = "B"
+
+print(points[(1, 2)])  #A
+print(points[(3, 4)])  #B                            
+
+print(points) #{(1, 2): 'A', (3, 4): 'B'}
