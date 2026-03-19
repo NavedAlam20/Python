7b622812915a5207fe44749e91765afb777befb9
Naved Alam <navedalam5665@gmail.com>
2026-03-19 23:43:07 +0530

    Python set is an unordered collection of multiple items having different datatypes

---
+++ Sets/Creating.py
@@ -0,0 +1,8 @@
+s = {1, 2, 3, 4}
+print(s) #{1, 2, 3, 4}
+print(type(s)) #<class 'set'>
+
+#using set function
+a = set("HelloPython")
+print(a) #{'P', 'n', 'h', 'l', 'o', 'e', 'H', 't', 'y'}
+print(type(a)) #<class 'set'>
