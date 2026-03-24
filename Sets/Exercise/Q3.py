db008adc1c7c1f8436a029d38be250e96e937226
Naved Alam <navedalam5665@gmail.com>
2026-03-24 22:08:11 +0530

    Convert set to list and string

---
+++ Sets/Exercise/Q3.py
@@ -0,0 +1,11 @@
+#Convert set into a list
+a = {1, 2, 3, 4, 5}
+b = list(a)
+print(b) #[1, 2, 3, 4, 5]
+
+#Convert Set to String
+
+c = {1, 2, 3}
+
+res = str(a) 
+print(type(res), res) #<class 'str'> {1, 2, 3}
