034d35a876b195679df0ff9e9a3c4e2b199a10ac
Naved Alam <navedalam5665@gmail.com>
2026-03-26 23:07:03 +0530

    Warning: This will eventually consume all memory

---
+++ Loops/Infinite_Loop.py
@@ -0,0 +1,8 @@
+while (True):
+    print("Hello Python") #Prints this until memory runs out
+
+# Using for loop
+my_list = [1]
+for x in my_list:
+    print(x) #prints 1 to infinity
+    my_list.append(x + 1)
