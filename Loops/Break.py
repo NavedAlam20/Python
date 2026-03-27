de54bec7736bdc8f1458a124a3b96428b74f4ea1
Naved Alam <navedalam5665@gmail.com>
2026-03-27 22:25:25 +0530

    Used to terminate loop when condition satisfied

---
+++ Loops/Break.py
@@ -0,0 +1,14 @@
+i = 0
+while (i <= 5):
+    print(i) #print 0 to 5 until 3 when i == 3 it terminates the loop
+    if (i == 3):
+        break
+    i += 1
+print("End of loop")
+
+'''
+1
+2
+3
+End of loop
+'''
