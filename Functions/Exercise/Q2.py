5f7a8f199d8c614a084c6a616d774108e9ca7076
Naved Alam <navedalam5665@gmail.com>
2026-03-31 23:23:08 +0530

    WAP to print the elemts of a list in a single line.(list is the parameter)

---
+++ Functions/Exercise/Q2.py
@@ -0,0 +1,10 @@
+x = [1,2,3,4,5,6]
+heroes = ["Iron Man", "Thor", "Captain America"]
+
+def print_list(list):
+    for i in list:
+        print(i,end=" ")
+
+print_list(heroes)
+print()
+print_list(x)
