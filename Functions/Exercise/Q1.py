bd530419cc89918ae586d9541aab754266d976cd
Naved Alam <navedalam5665@gmail.com>
2026-03-31 23:21:29 +0530

    WAP to print the length of a list.(List is the parameter)

---
+++ Functions/Exercise/Q1.py
@@ -0,0 +1,8 @@
+x = [1,2,3,4,5,6]
+heroes = ["Iron Man", "Thor", "Captain America"]
+
+def print_len(list):
+    print("Length of a list is:",len(list))
+
+print_len(heroes) #Length of a list is: 3
+print_len(x) #Length of a list is: 6
