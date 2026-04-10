72907fb3349c96ac0ea9c9d7236242b6f812eece
Naved Alam <navedalam5665@gmail.com>
2026-04-10 23:40:57 +0530

    This creates a new file if it doesn’t exist, or overwrites the existing file if it does

---
+++ File_Handling/Writing.py
@@ -0,0 +1,4 @@
+f = open("Demo.txt","w")
+f.write("Hello I Am Learning Python\n")#this overwrite the file and in end we use \n to go to next line
+f.write("In Python I Am Learning File Handling")#this add to next line because of \n
+f.close()
